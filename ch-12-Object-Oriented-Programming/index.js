fbb647446fbea74653a55f3177d218cdbc53682c
masudfcs1 <masudfcs1@gmail.com>
2022-09-11 10:50:14 +0600

    this

--- ch-12-Object-Oriented-Programming/index.js
+++ ch-12-Object-Oriented-Programming/index.js
@@ -4,9 +4,11 @@ const react = {
 
     draw: function() {
         console.log('Never Give Up.');
+        console.log('Talk About Height ' + this.height);
+        console.log('Talk About Weight ' + this.weight);
     }
 
 
 }
 
-react.draw();
+react.draw()
