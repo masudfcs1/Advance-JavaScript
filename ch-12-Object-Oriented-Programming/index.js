9ade816b7a17d61212b1538891758936e05c1129
masudfcs1 <masudfcs1@gmail.com>
2022-09-13 07:20:07 +0600

    add line

--- ch-12-Object-Oriented-Programming/index.js
+++ ch-12-Object-Oriented-Programming/index.js
@@ -4,6 +4,12 @@ const react = {
 
     draw: function() {
         console.log('Never Give Up.');
+        this.propartice()
+        console.log(this);
+
+    },
+
+    propartice: function() {
         console.log('Talk About Height ' + this.height);
         console.log('Talk About Weight ' + this.weight);
     }
@@ -14,3 +20,12 @@ const react = {
 react.draw()
 react.weight = 80
 react.draw()
+
+
+var another = {
+    weight: 10,
+    height: 20,
+    print: react.propartice
+}
+
+another.print()
